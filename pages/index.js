05a0fd7eea2b63832befa65779ea65843159fffd
JiYoungWook <https://github.com/KooSinB/next-shop>
2022-12-12 11:37:20 +0900

    majimak

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,20 @@ import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
-  <div className={styles.container}>
-    <Head>
-      <title>Home</title>
+    <div className={styles.container}>
+      <Head>
+        <title>Home</title>
       </Head>
+
       <div className={styles.main}>
-      <h1 className={styles.homeTitle}>Welcome Home</h1>
-      <Link href="about"><h3>About</h3></Link>
-      <Link href="about"><h3>Profile</h3></Link>
+        <h1 className={styles.homeTitle}>Welcome Home</h1>
+        <Link href="about">
+          <h3>About</h3>
+        </Link>
+        <Link href="about">
+          <h3>Profile</h3>
+        </Link>
       </div>
-      </div>
-      )
+    </div>
+  )
 }
